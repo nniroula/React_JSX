2d9de16ebacd4a0ffbdbfafb020ce9bc5469dca0
nabin <nkbhaie2017@gmail.com>
2022-01-28 08:21:26 -0700

    add first react component

--- react-jsx-intro/part-1/FirstComponent.js
+++ react-jsx-intro/part-1/FirstComponent.js
@@ -2,4 +2,4 @@ const FirstComponent = ()=>{
     return <h1>My very first component</h1>
 }
 // add this component ot DOM
-ReactDOM.render(<FirstComponent/>, document.getElementById("root"))
+// ReactDOM.render(<FirstComponent/>, document.getElementById("root"))
